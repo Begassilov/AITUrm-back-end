d088d91ba3f9ca5598a0abdb42af0997e0074009
Nurs <begasilovi4@gmail.com>
2023-01-13 19:20:03 +0600

    Add all my files

---
+++ src/main/java/com/example/AITUrm_backend/AitUrmBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.AITUrm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AitUrmBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AitUrmBackEndApplication.class, args);
+	}
+
+}
